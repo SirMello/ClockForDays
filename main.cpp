9a0906331f99a58d3d0a656c3f8e90c9d16aa260
Ahmed Imana <ahmedoy@icloud.com>
2018-07-10 18:09:23 -0400

    FIXX!

--- main.cpp
+++ main.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
 #include <string>
-#include <ctime>
+#include <chrono>
+#include <thread>
+#include "clock.h"
 
-#include "Clock.h"
+void sleep(int x){
+  std::this_thread::sleep_for(std::chrono::milliseconds(x));
+}
+
+
+
 using namespace std;
 
 int setHour(Clock clc){
@@ -10,8 +17,14 @@ int setHour(Clock clc){
     cout << "Input the current hour." << endl;
     cin >> clc.hour;
   }
-
 }
+
+
+void showTime(Clock clc){
+  string time;
+  time = clc.hour + ":" + clc.minute + ":" + clc.seconds;
+}
+
 
 int setMinute(Clock clc){
   while (clc.minute > 59){
@@ -23,8 +36,29 @@ int setMinute(Clock clc){
 
 int main(){
   Clock clock;
-
+  bool done = false;
   setHour(clock);
   setMinute(clock);
+
+  while (!done){
+    sleep(10);
+    ++clock.second;
+    if (clock.second == 60){
+      clock.second = 0;
+      ++clock.minute;
+    }
+    if (clock.minute == 60){
+        clock.minute = 0;
+        ++clock.hour;
+      }
+
+    if (clock.hour == 25){
+      clock.hour = 0;
+      clock.minute = 0;
+      clock.second = 0;
+      }
+    }
+
+
   return 0;
 }
