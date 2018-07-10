14c4bb3d624cca69d4f4174dcd6a38ee221f9812
Ahmed Imana <ahmedoy@icloud.com>
2018-07-10 10:25:58 -0400

    redoing function (setTime)

--- Clock.cpp
+++ Clock.cpp
@@ -1,26 +1,12 @@
 #include <iostream>
 #include <string>
+#include <ctime>
 using namespace std;
 
 string globalTime("");
 
 void setTime(){
-  bool done = false;
-  while(!done){
-    string time;
-    cout << "Please enter a starting time. Press ENTER when done." << endl;
-    cin >> time;
-    if (time.length() <= 5){
-      for(auto c = time.begin(); c != time.end(); ++c){
-        if (*c == ':'){
-          globalTime = time;
-          done = true;
-          cout << "Starting Time: " << globalTime << endl;
-          break;
-        }
-      }
-    }
-  }
+  
 }
 
 int main(){
