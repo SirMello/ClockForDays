c60b2485f39ffad57f1279ef55c2615016fded78
Ahmed Imana <ahmedoy@icloud.com>
2018-07-11 14:40:13 -0400

    moved all functions into clock.h class

--- Clock.h
+++ Clock.h
@@ -1,8 +1,55 @@
 #include <iostream>
+#include <string>
 
 class Clock {
+private:
+  void clearScreen(){
+    int n;
+    for (n = 0; n < 10; ++n){
+      std::printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+    }
+  }
 public:
   int hour = 25;
   int minute = 60;
   int second = 0;
+
+public:
+  void setMinute(){
+    while (minute > 59){
+      std::cout << "Input the current minute." << std::endl;
+      std::cin >> minute;
+    }
+  }
+
+  void setHour(){
+    while (hour > 24){
+      std::cout << "Input the current hour." << std::endl;
+      std::cin >> hour;
+    }
+  }
+
+  void showTime(){
+    std::string time;
+    if (std::to_string(hour).length() == 1){
+      time = "0" + std::to_string(hour);
+    } else {
+      time = std::to_string(hour);
+    }
+
+    if (std::to_string(minute).length() == 1){
+      time += ": 0" + std::to_string(minute);
+    } else {
+      time += ": " + std::to_string(minute);
+    }
+
+    if (std::to_string(second).length() == 1){
+      time += ": 0" + std::to_string(second);
+    } else {
+      time += ": " + std::to_string(second);
+    }
+
+    clearScreen();
+    std::cout << time << std::flush;
+    }
 };
--- main.cpp
+++ main.cpp
@@ -7,60 +7,15 @@
 void sleep(int x){
   std::this_thread::sleep_for(std::chrono::milliseconds(x));
 }
+int ONE_SECOND = 1000;
 
 using namespace std;
-int setHour(Clock clc){
-  while (clc.hour > 24){
-    cout << "Input the current hour." << endl;
-    cin >> clc.hour;
-  }
-  return clc.hour;
-}
-
-void clearScreen(){
-  int n;
-  for (n = 0; n < 10; ++n){
-    printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
-  }
-}
-
-int setMinute(Clock clc){
-  while (clc.minute > 59){
-    cout << "Input the current minute." << endl;
-    cin >> clc.minute;
-  }
-  return clc.minute;
-}
-
-void showTime(Clock clc){
-  string time;
-  if (to_string(clc.hour).length() == 1){
-    time = "0" + to_string(clc.hour);
-  } else {
-    time = to_string(clc.hour);
-  }
-
-  if (to_string(clc.minute).length() == 1){
-    time += ": 0" + to_string(clc.minute);
-  } else {
-    time += ": " + to_string(clc.minute);
-  }
-
-  if (to_string(clc.second).length() == 1){
-    time += ": 0" + to_string(clc.second);
-  } else {
-    time += ": " + to_string(clc.second);
-  }
-
-  clearScreen();
-  cout << time << flush;
-}
 
 int main(){
   Clock clock;
   bool done = false;
-  clock.hour = setHour(clock);
-  clock.minute = setMinute(clock);
+  clock.setHour();
+  clock.setMinute();
 
   while (!done){
     sleep(ONE_SECOND);
@@ -79,7 +34,7 @@ int main(){
       clock.minute = 0;
       clock.second = 0;
       }
-    showTime(clock);
+    clock.showTime();
     }
 
 
